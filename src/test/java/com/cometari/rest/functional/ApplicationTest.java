faa1c5c664004cf9478a19b8d879fc7b1c017478
Marcin Kordus <marcin.kordus8@gmail.com>
2016-02-13 14:22:26 +0100

    first functional test

---
+++ src/test/java/com/cometari/rest/functional/ApplicationTest.java
@@ -0,0 +1,48 @@
+package com.cometari.rest.functional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.cometari.rest.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class ApplicationTest {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+ 
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders
+		    .webAppContextSetup(webApplicationContext)
+            .build();
+	}
+
+    @Test
+    public void checkGetWorks() throws Exception {
+        mockMvc.perform(get("/users"))
+            .andExpect(status().isOk())
+            .andExpect(
+                content().contentType(MediaType.APPLICATION_JSON_VALUE)
+            );
+    }
+    
+}
