9ca0d10335128ae8229eae91236eed166ef3162d
Marcin Kordus <marcin.kordus8@gmail.com>
2016-02-13 15:22:10 +0100

    add UserRepository

---
+++ src/main/java/com/cometari/rest/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cometari.rest.repository;
+
+import com.cometari.rest.domain.User;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+}
