42b61ed29c2a0db88559d60176713915baa621fd
Marcin Kordus <marcin.kordus8@gmail.com>
2016-02-14 21:39:03 +0100

    add spring-rest-exception-handler

---
+++ src/main/java/com/cometari/rest/RestContextConfig.java
@@ -0,0 +1,53 @@
+package com.cometari.rest;
+
+import java.util.List;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
+
+import cz.jirutka.spring.exhandler.RestHandlerExceptionResolver;
+import cz.jirutka.spring.exhandler.support.HttpMessageConverterUtils;
+
+@EnableWebMvc
+@Configuration
+public class RestContextConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add( exceptionHandlerExceptionResolver() ); // resolves @ExceptionHandler
+        resolvers.add( restExceptionResolver() );
+    }
+
+    @Bean
+    public RestHandlerExceptionResolver restExceptionResolver() {
+        return RestHandlerExceptionResolver.builder()
+                .messageSource( httpErrorMessageSource() )
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .addErrorMessageHandler(EmptyResultDataAccessException.class, HttpStatus.NOT_FOUND)
+                .build();
+    }
+
+    @Bean
+    public MessageSource httpErrorMessageSource() {
+        ReloadableResourceBundleMessageSource m = new ReloadableResourceBundleMessageSource();
+        m.setBasename("classpath:/org/example/messages");
+        m.setDefaultEncoding("UTF-8");
+        return m;
+    }
+
+    @Bean
+    public ExceptionHandlerExceptionResolver exceptionHandlerExceptionResolver() {
+        ExceptionHandlerExceptionResolver resolver = new ExceptionHandlerExceptionResolver();
+        resolver.setMessageConverters(HttpMessageConverterUtils.getDefaultHttpMessageConverters());
+        return resolver;
+    }
+}
