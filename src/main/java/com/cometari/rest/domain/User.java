39cd91f87551045659f7ce9437cf38313204b789
Marcin Kordus <marcin.kordus8@gmail.com>
2016-02-14 16:30:54 +0100

    adapt User entity

--- src/main/java/com/cometari/rest/domain/User.java
+++ src/main/java/com/cometari/rest/domain/User.java
@@ -1,20 +1,41 @@
 package com.cometari.rest.domain;
 
+import com.cometari.rest.constraints.Email;
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
-public class User {
+@Table(name = "users")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @Size(min = 3, max = 80)
 	private String firstName;
+
+    @Size(min = 3, max = 80)
 	private String lastName;
+
+    @Email
+	@NotNull
+	@Column(unique = true)
 	private String emailAddress;
-	private Boolean isActivated;
+
+	@NotNull
+	private boolean isActivated;
+
+	@NotNull
 	private String passwordHash;
 
 	public User() {}
